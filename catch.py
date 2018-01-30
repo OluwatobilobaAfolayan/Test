a8d84dd9e9ecff5b92683e7d2a9674db6ece6103
Emmanuel Afolayan <tea29@txstate.edu>
2018-01-30 14:01:24 -0800

    changes to catch

--- catch.py
+++ catch.py
@@ -1,9 +1,2 @@
 from pwn import * 
-import base64
-with open("given.txt", "r") as f:
-    for line in f:
-  	#with open("output.txt", "a") as out:
-	temp = base64.b64decode(line)
-        #out.close()
-        x = temp[4:8]
-	print(x)
+
